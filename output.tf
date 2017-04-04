9d65489247c9d374267168bd616ca7f2705b682d
Erik Stidham <erik@tigera.io>
2017-04-04 11:28:37 -0700

    Output master IP when finished too

--- output.tf
+++ output.tf
@@ -2,6 +2,10 @@
 #  value = "${google_compute_forwarding_rule.default.ip_address}"
 #}
 
+output "master_ip" {
+  value = "${google_compute_instance.calico-master.network_interface.0.access_config.0.assigned_nat_ip}"
+}
+
 output "instance_ips" {
   value = "${join(" ", google_compute_instance.calico.*.network_interface.0.access_config.0.assigned_nat_ip)}"
 }
